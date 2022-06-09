ed207db9a875d47fc0464db7deb2212f69e4f994
sjperez1 <sarahpz479@gmail.com>
2022-06-09 16:43:15 -0600

    updated comments and naming

--- users_with_bank_accounts.py
+++ users_with_bank_accounts.py
@@ -1,6 +1,6 @@
 # This code shows if each user had two bank accounts.
-
-class bank_account:
+# Classes need to be capitalized
+class Bank_Account:
     # whatever is put here is a global variable
     all_bank_accounts = []
     # classmethod is used when you are accessing something global (directly under class), whereas you can use regular def if it is not something global
@@ -10,20 +10,21 @@ class bank_account:
         # instance attributes go here
         self.int_rate = int_rate
         self.balance = balance
-        bank_account.all_bank_accounts.append(self)
+        Bank_Account.all_bank_accounts.append(self)
     def deposit(self, amount):
         self.balance += amount
         return self
     def withdraw(self, amount):
         self.balance -= amount
         return self
-    def display_account_info(self):
-        print(f"Balance: ${self.balance}")
-        return self
-    def yield_interest(self):
-        if self.balance > 0:
-            self.balance = self.balance + (self.balance * self.int_rate)
-            return self
+    # display_account_info() and yield_interest were used when I was just doing bank account
+    # def display_account_info(self):
+    #     print(f"Balance: ${self.balance}")
+    #     return self
+    # def yield_interest(self):
+    #     if self.balance > 0:
+    #         self.balance = self.balance + (self.balance * self.int_rate)
+    #         return self
     # commenting out the following class method because it is not too necessary to have in this scenario, but there is a working one on the other file with one account per user.
     # @classmethod
     # def all_bank_account_balances(cls):
@@ -31,16 +32,16 @@ class bank_account:
     #         print(f"Balance: ${each_account.balance}")
         # Chain methods of an instance (not a class), so nothing similar to return self is needed. In addition, the goal is just to print here.
 
-# account1 and account2 do not have a user attached to the accounts, so they are not really doing anything too significant now that we have users liked to bank account in class User. Therefore, we can get rid of (I'll comment them out) the bank accounts created, the related chains, and bank_account.all_bank_account_balances() since we will not have accounts to put in the list.
-# account1 = bank_account(0.045, 200)
-# account2 = bank_account(0.04, 650)
+# account1 and account2 do not have a user attached to the accounts, so they are not really doing anything too significant now that we have users liked to bank account in class User. Therefore, we can get rid of (I'll comment them out) the bank accounts created, the related chains, and Bank_Account.all_bank_account_balances() since we will not have accounts to put in the list.
+# account1 = Bank_Account(0.045, 200)
+# account2 = Bank_Account(0.04, 650)
 
 # # for the methods where there is nothing in parentheses, we are only passing in the self value.
 # account1.deposit(433).deposit(153).deposit(48).withdraw(198).yield_interest().display_account_info()
 # account2.deposit(212).deposit(119).withdraw(13).withdraw(22).withdraw(29).withdraw(46).yield_interest().display_account_info()
 
 # the following corresponds to the classmethod that is commented out.
-# bank_account.all_bank_account_balances()
+# Bank_Account.all_bank_account_balances()
 
 
 class User:
@@ -48,17 +49,17 @@ class User:
     def __init__(self, name, savings_balance, checking_balance):
         self.name = name
         # account balance already has a default value, so we do not have to include that as a parameter. If the default wasn't zero, we would have to include it as a parameter.
-        # Creating a new instance of bank account, like account1 = bank_account(0.045, 200). Make a dictionary to store the info multiple bank accounts.
+        # Creating a new instance of bank account, like account1 = Bank_Account(0.045, 200). Make a dictionary to store the info multiple bank accounts.
         self.account = {
-            "savings":bank_account(0.012, balance=savings_balance),
-            "checking":bank_account(0.012, balance=checking_balance)
+            "savings":Bank_Account(0.012, balance=savings_balance),
+            "checking":Bank_Account(0.012, balance=checking_balance)
         }
         User.all_users.append(self)
     
-    # bank_account does not have name in the class attribute, so you would keep in user.
+    # Bank_Account does not have name in the class attribute, so you would keep in user.
     def display_user_balance(self):
-        for type,info in self.account.items():
-            print(f"{self.name}'s {type} Account Balance: ${info.balance}")
+        for key,info in self.account.items():
+            print(f"{self.name}'s {key} Account Balance: ${info.balance}")
         return self
 
     # The following is for more practice. These are the following lines that correspond to this method: all_users = [ ], User.all_users.append(self), User.all_user_balances()
@@ -76,6 +77,7 @@ catherine = User("Catherine Smith", 90, 62)
 sally = User("Sally Miller", 120, 129)
 
 # Looking at the parameters in make_deposit and make_withdrawal, we are passing sarah (what sarah is equal to) in as an argument for self and passing the numeric value in parentheses as an argument for amount
+# Another way to do this assignment is to keep the methods from the user assignment and just call the deposit and withdraw methods from Bank_Account, then call the methods from User down here.
 sarah.account["savings"].deposit(325).deposit(642).deposit(122).withdraw(236)
 sarah.account["checking"].deposit(460).deposit(502).deposit(642).withdraw(98)
 sarah.display_user_balance()
@@ -89,4 +91,4 @@ sally.account["checking"].deposit(895).withdraw(88).withdraw(56).withdraw(65)
 sally.display_user_balance()
 
 User.all_user_balances()
-# inside of the user has a bank_account instance
+# inside of the user has a Bank_Account instance
